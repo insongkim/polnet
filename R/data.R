2d3fda63fcd06f4c61c833ef0c6cf7625a04a0c3
Henry <hshackle@mit.edu>
2018-05-29 03:04:45 -0400

    Added biLCM data generator, house voting data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' 115th House of Representatives Voting Data
+#'
+#' A dataset containing the voting records of House of Representative
+#' members of the 115th Congress
+#'
+#' @format A data frame with 376 rows and 672 variables:
+#' \describe{
+#'   \item{vote_X}{Vote on the X'th bill (1=yay, 0=nay)}
+#'   ...
+#' }
+"house-data"
