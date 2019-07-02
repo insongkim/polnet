eabae7cb75528871524a2e47863ba63624cc56f2
Sooahn Shin <anne940303@gmail.com>
2019-07-02 09:52:37 +0900

    clean up biLCM function

--- R/biLCM.R
+++ R/biLCM.R
@@ -1,4 +1,4 @@
-#'@name biLSM
+#'@name biLCM
 #'@param A Matrix of connection strength as counts
 #'@param m Number of clients
 #'@param n Number of politicians
@@ -7,104 +7,82 @@
 
 #'@export
 
-biLCM <- function(A, m, n, k){
+biLCM <- function(edges, 
+                  group1.id = NULL,
+                  group2.id = NULL,
+                  count.id = NULL
+                  k = NULL,
+                  seed = 12345,
+                  tolerance = 1e-6,
+                  max.iter = 200){
+  
+  ## Warning for missing parameter
+  if (missing(edges))
+    stop("'edges' should be provided")
+  if (!class(edges)%in%c("matrix","data.frame", "igraph"))
+    stop("'edges' should be matrix or data.frame or igraph")
+  if (class(edges)=="data.frame"&is.null(group1.id))
+    stop("'group1.id' should be provided")
+  if (class(edges)=="data.frame"&is.null(group2.id))
+    stop("'group2.id' should be provided")
+  if (class(edges)%in%c("data.frame", "igraph")&is.null(count.id))
+    stop("'count.id' should be provided")
+  if (is.null(k))
+    stop("'k' should be provided")
+  
+  # Input data
+  if (class(edges)=="matrix") {
+    edge_mat <- edges
+    edge_mat[is.na(edge_mat)] <- 0
+  } else {
+    
+    if (class(edges)=="igraph") {
+      edges <- igraph::as_data_frame(edges, what = "edges")
+      group1.id <- "from"
+      group2.id <- "to"
+    }
+    
+    edges <- edges[,c(group1.id, group2.id, count.id)]
+    edge_mat <- tidyr::spread(edges, group2.id, count.id)
+    rownames(edge_mat) <- edge_mat[,group1.id]
+    edge_mat <- edge_mat[,-1]
+    edge_mat <- as.matrix(edge_mat)
+    edge_mat[is.na(edge_mat)] <- 0
+  } 
+  
+  m <- nrow(edge_mat)
+  n <- ncol(edge_mat)
+  
+  # Initialize parameters
+  set.seed(seed)
+  kappa <- rgamma(k, 2, 0.5)
+  alpha <- matrix(runif(m*k), m, k)
+  alpha[,] <- alpha/rowSums(alpha)
+  beta <- matrix(runif(n*k), n, k)
+  beta[,] <- beta/rowSums(beta)
+  q <- array(0, c(m, n, k))
+  
+  iter_count <- 0
+  llik_iter <- numeric(max.iter)
+  
+  repeat {
+    iter_count <- iter_count + 1
+    # E-step
 
-loglike_old <- 1
-loglike_current <- 0
-converge <- .000001
+    
+    # M-step
 
-# Initializes variables
-
-q <- array(1, c(m,n,k)) 
-kappa <- array(runif(k, min = 1, max = 10), k)
-alpha <-  array(runif(m*k, min = 1, max = 10), c(m,k))
-for (z in 1:k){
-	alpha[, z] <- alpha[, z]/sum(alpha[, z])
-}
-
-beta <-  array(runif(n*k, min = 1, max = 10), c(n,k))
-for (z in 1:k){
-	beta[, z] <- beta[, z]/sum(beta[, z])
-}
-
-# Begins iterative procedure, stops when desired convergence has been reached
-
-while (abs(loglike_current - loglike_old) > converge) {
-	
-	loglike_old <- loglike_current	
-	
-	# Update q
-	for (i in 1:m){
-		for (j in 1:n){
-			for (z in 1:k){
-				q[i, j, z] <- kappa[z]*alpha[i,z]*beta[j,z]
-			}
-			q[i, j, ] <- q[i, j, ]/sum(q[i, j, ])
-		}
-		
-	}
-	
-	# Update kappa
-	for (z in 1:k){
-		numer <- 0
-		denom <- 0
-		for (i in 1:m) {			
-			for (j in 1:n) {
-				numer <- numer + A[i, j]*q[i, j, z]
-				denom <- denom + alpha[i, z]*beta[j, z]
-			}
-		} 
-		kappa[z] <- numer/denom
-	}
-	# Update alpha
-	for (i in 1:m){
-		for (z in 1:k){
-			alpha[i, z] <- 0
-			for (j in 1:n){
-				alpha[i, z] <- alpha[i, z] + A[i, j]*q[i, j, z]
-			}
-			
-		}
-	}
-	for (z in 1:k){
-		alpha[, z] <- alpha[, z]/sum(alpha[, z])
-	}
-	# Update beta
-	for (j in 1:n){
-		for (z in 1:k){
-			beta[j, z] <- 0
-			for (i in 1:m){
-				beta[j, z] <- beta[j, z] + A[i, j]*q[i, j, z]
-			}
-			
-		}
-	}
-	for (z in 1:k){
-		beta[, z] <- beta[, z]/sum(beta[, z])
-	}
-	
-	# Calculate log likelihood
-	t1 <- 0
-	for (i in 1:m) {
-		for (j in 1:n) {
-			t2 <- 0
-			for (z in 1:k) {
-				t2 <- t2 + kappa[z]*alpha[i, z]*beta[j, z]
-			}
-			t1 <- t1+ A[i, j]*t2
-		}
-	}
-	t3 <- 0
-	for (i in 1:m) {
-		for (j in 1:n) {
-			for (z in 1:k) {
-				t3 <- t3 + kappa[z]*alpha[i, z]*beta[j, z]
-			}
-		}
-	}
-	loglike_current <- t1 - t3
-}
-
+    
+    # Calculate log likelihood
+    llik_new <- 0
+    llik_iter[iter_count] <- llik_new
+    
+    ## check convergence
+    if (llik_new - llik_old < tolerance || iter_count == max.iter) break
+    ## update log-likelihood
+    llik_old <- llik_new
+  }
+    
 	answer <- list(kappa = kappa, alpha = alpha, beta = beta)
 	return(answer)
 }
